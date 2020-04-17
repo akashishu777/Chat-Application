a4e98656463696df90b1cbd914d72c5c2aa5d0ec
Akash Vishwakarma <akash.vishwakarma@milliman.com>
2020-04-17 22:48:44 +0530

    Changed the folder structure, update the UI to look like messenger, now serving the html from server sise

---
+++ client/main.js
@@ -0,0 +1,26 @@
+var socket = io();
+
+var form = document.querySelector('form');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  var input = document.querySelector('#message');
+  var text = input.value;
+  socket.emit('chat message', text);
+  input.value = '';
+});
+
+socket.on('chat message', function(text) {
+  if (!text) {
+    return;
+  }
+  var container = document.querySelector('section');
+  var newMessage = document.createElement('p');
+  newMessage.innerText = text;
+  container.appendChild(newMessage);
+
+  var seperator = document.createElement('br');
+  container.appendChild(seperator);
+
+  container.scrollTop = container.scrollHeight;
+});
--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var PORT = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+
+var http = require('http');
+var server = http.createServer(app);
+
+app.use(express.static('client'));
+
+var io = require('socket.io')(server);
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -8,12 +16,11 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
-      console.log(msg);
       io.emit('chat message', msg);
     });
   });
 
-http.listen(process.env.PORT || 3000, () => {
-    console.log('listning on *3000');
+server.listen(process.env.PORT || PORT, () => {
+    console.log('listning on 3000');
 });
 
