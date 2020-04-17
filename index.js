2cede19ccadefb429572c4dccae3df4a55b1845c
Akash Vishwakarma <akash.vishwakarma@milliman.com>
2020-04-17 20:44:04 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      console.log(msg);
+      io.emit('chat message', msg);
+    });
+  });
+
+http.listen(3000, () => {
+    console.log('listning on *3000');
+});
+
